3aa9056904fcec1a957628c978b4fabeb246d7c1
Corno Schraverus <corno@schraverus.com>
2026-02-06 13:23:42 +0100

    pdt: ...

--- pub/src/interface/to_be_generated/deserialize.ts
+++ pub/src/interface/to_be_generated/deserialize.ts
@@ -1,11 +1,11 @@
 import * as _pi from 'pareto-core/dist/interface'
 
-import * as d_parse_result from "astn-core/dist/interface/generated/liana/schemas/deserialize_parse_tree/data"
+import * as d_deserialized_parse_tree from "astn-core/dist/interface/generated/liana/schemas/deserialize_parse_tree/data"
 import * as d_unmarshall from "./unmarshall"
 
 
 export type Error_ = 
-    | ['parse error', d_parse_result.Error]
+    | ['parse error', d_deserialized_parse_tree.Error]
     | ['unmarshall error', d_unmarshall.Error]
 
 export { 
