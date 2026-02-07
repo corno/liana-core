4996a22d713ad76426b5cce16cef4651ded468da
Corno Schraverus <corno@schraverus.com>
2026-02-07 21:02:16 +0100

    pdt: ...

--- pub/src/implementation/manual/primitives/integer/serializers/fractional_decimal.ts
+++ pub/src/implementation/manual/primitives/integer/serializers/fractional_decimal.ts
@@ -1,7 +1,6 @@
 import * as _p from 'pareto-core/dist/assign'
 import _p_unreachable_code_path from 'pareto-core/dist/_p_unreachable_code_path'
 import _p_list_build_deprecated from 'pareto-core/dist/_p_list_build_deprecated'
-import _p_text_from_list from 'pareto-core/dist/_p_text_from_list'
 
 import * as signatures from "../../../../../interface/signatures"
 
