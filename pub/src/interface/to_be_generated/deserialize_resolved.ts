eb3a213c20153e728979a86932c57ff1e2479834
Corno Schraverus <corno@schraverus.com>
2026-02-08 12:51:37 +0100

    pdt: published version 0.0.18

--- pub/src/interface/to_be_generated/deserialize_resolved.ts
+++ pub/src/interface/to_be_generated/deserialize_resolved.ts
@@ -6,3 +6,5 @@ import * as d_deserialize from "./deserialize"
 export type Error =
     | ['deserialize', d_deserialize.Error]
     | ['resolve error', d_resolve.Error]
+
+export type Parameters = d_deserialize.Parameters
