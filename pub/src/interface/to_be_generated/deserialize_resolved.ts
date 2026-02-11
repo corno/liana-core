fee0c9affbbd8f64d18b260a560e63da99cdcfa8
Corno Schraverus <corno@schraverus.com>
2026-02-11 19:46:36 +0100

    pdt: published version 0.0.23

--- pub/src/interface/to_be_generated/deserialize_resolved.ts
+++ pub/src/interface/to_be_generated/deserialize_resolved.ts
@@ -1,6 +1,6 @@
 
 import * as d_resolve from "./resolve"
-import * as d_deserialize from "./deserialize"
+import * as d_deserialize from "astn-core/dist/interface/to_be_generated/deserialize"
 
 
 export type Error =
