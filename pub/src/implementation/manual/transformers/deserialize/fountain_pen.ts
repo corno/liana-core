35458c48c8a05d7462642e73fba692e786d978be
Corno Schraverus <corno@schraverus.com>
2026-02-26 15:48:25 +0100

    pdt: published version 0.0.36

--- pub/src/implementation/manual/transformers/deserialize/fountain_pen.ts
+++ pub/src/implementation/manual/transformers/deserialize/fountain_pen.ts
@@ -1,17 +1,19 @@
 import * as _p from 'pareto-core/dist/assign'
+import * as _pi from 'pareto-core/dist/interface'
 
 //data types
 import * as d_in from "../../../../interface/to_be_generated/deserialize"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+import * as d_function_loc from "astn-core/dist/interface/to_be_generated/location_to_fountain_pen"
 
 //dependencies
 import * as t_deserialize_parse_tree_to_fountain_pen from "astn-core/dist/implementation/manual/transformers/deserialize_parse_tree/fountain_pen"
 import * as t_unmarshall_to_fountain_pen from "../unmarshall/fountain_pen"
 
-export const Error = ($: d_in.Error): d_out.Phrase => _p.decide.state($, ($) => {
+export const Error: _pi.Transformer_With_Parameter<d_in.Error, d_out.Phrase, d_function_loc.Parameters> = ($, $p) => _p.decide.state($, ($) => {
     switch ($[0]) {
 
-        case 'parse error': return _p.ss($, ($) => t_deserialize_parse_tree_to_fountain_pen.Error($, { 'position info': ['zero based', null] }))
+        case 'parse error': return _p.ss($, ($) => t_deserialize_parse_tree_to_fountain_pen.Error($, $p))
         case 'unmarshall error': return _p.ss($, ($) => t_unmarshall_to_fountain_pen.Error($))
         default: return _p.au($[0])
     }
--- pub/src/implementation/manual/transformers/deserialize_resolved/fountain_pen.ts
+++ pub/src/implementation/manual/transformers/deserialize_resolved/fountain_pen.ts
@@ -1,17 +1,19 @@
 import * as _p from 'pareto-core/dist/assign'
+import * as _pi from 'pareto-core/dist/interface'
 
 //data types
 import * as d_in from "../../../../interface/to_be_generated/deserialize_resolved"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+import * as d_function_loc from "astn-core/dist/interface/to_be_generated/location_to_fountain_pen"
 
 //dependencies
 import * as t_resolve_to_fountain_pen from "../resolve/fountain_pen"
 import * as t_deserialize_to_fountain_pen from "../deserialize/fountain_pen"
 
-export const Error = ($: d_in.Error): d_out.Phrase => _p.decide.state($, ($) => {
-    switch ($[0]) {
 
-        case 'deserialize': return _p.ss($, ($) => t_deserialize_to_fountain_pen.Error($))
+export const Error: _pi.Transformer_With_Parameter<d_in.Error, d_out.Phrase, d_function_loc.Parameters> = ($, $p) => _p.decide.state($, ($) => {
+    switch ($[0]) {
+        case 'deserialize': return _p.ss($, ($) => t_deserialize_to_fountain_pen.Error($, $p))
         case 'resolve error': return _p.ss($, ($) => t_resolve_to_fountain_pen.Error($))
         default: return _p.au($[0])
     }
