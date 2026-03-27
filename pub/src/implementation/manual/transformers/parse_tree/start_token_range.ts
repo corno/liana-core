016971455a78cf165d01526a351cb01f4f39a927
Corno Schraverus <corno@schraverus.com>
2026-03-27 10:33:55 +0100

    pdt: published version 0.1.11

---
+++ pub/src/implementation/manual/transformers/parse_tree/start_token_range.ts
@@ -0,0 +1,16 @@
+import * as _pi from 'pareto-core/dist/interface'
+
+import * as d_in from "astn-core/dist/interface/generated/liana/schemas/parse_tree/data"
+import * as d_out from "../../../../interface/to_be_generated/location"
+
+import * as t_pt_to_str from "astn-core/dist/implementation/manual/transformers/parse_tree/start_token_range"
+
+
+export const Value: _pi.Transformer_With_Parameter<d_in.Value, d_out.Range, { 'document resource identifier': string }> = ($, $p) => {
+    const x = t_pt_to_str.Value($)
+    return {
+        'document resource identifier': $p['document resource identifier'],
+        'start': x.start,
+        'end': x.end,
+    }
+}
