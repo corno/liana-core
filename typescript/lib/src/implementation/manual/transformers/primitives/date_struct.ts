2669044a0f51869d7123c928a3bbf5a9310fa1d0
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:51:06 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/primitives/date_struct.ts
+++ typescript/lib/src/implementation/manual/transformers/primitives/date_struct.ts
@@ -2,7 +2,7 @@ import * as p_ from 'pareto-core/implementation/transformer'
 import type * as p_i from 'pareto-core/interface/transformer'
 import p_unreachable_code_path from 'pareto-core/implementation/transformer/specials/unreachable_code_path'
 
-export type Date_Struct = {
+type Date_Struct = {
     year: number
     month: number
     day: number
