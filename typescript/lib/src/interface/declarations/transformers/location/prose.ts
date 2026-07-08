b33ced41d67bf72a2bd603843c9e1e32b4f19945
Corno Schraverus <corno@schraverus.com>
2026-07-08 21:30:07 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/location/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/location/prose.ts
@@ -6,14 +6,6 @@ import type * as d_in from "../../../data/document_and_location.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
 import type * as d_function from "astn-core/interface/data/location_to_prose"
 
-//dependencies
-import * as t_astn_location_to_prose from "astn-core/implementation/manual/transformers/location/prose"
-
-//shorthands
-import * as sh from "pareto-fountain-pen/shorthands/prose/deprecated"
-
-
-
 export type Range = p_.Transformer_With_Parameter<
     d_in.Range,
     d_out.Phrase,
